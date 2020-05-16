56212cd0779d2496769bf9f61437086e688c0ca8
duhone <duhone@outlook.com>
2020-05-16 17:19:29 -0500

    disable a few more clang-tidy checks

--- common.cmake
+++ common.cmake
@@ -75,7 +75,7 @@ function(settingsCR target)
 		# Use clangtidy
 		VS_GLOBAL_EnableClangTidyCodeAnalysis true
 		VS_GLOBAL_ClangTidyChecks "-checks=-*,modernize-*, -modernize-avoid-c-arrays, -modernize-use-trailing-return-type, \
-bugprone-*, -bugprone-bool-pointer-implicit-conversion, cppcoreguidelines-*, -cppcoreguidelines-avoid-c-arrays, misc-*, performance-*, readability-*, -readability-uppercase-literal-suffix"
+bugprone-*, -bugprone-bool-pointer-implicit-conversion, cppcoreguidelines-*, -cppcoreguidelines-avoid-c-arrays, -cppcoreguidelines-pro-bounds-constant-array-index, misc-*, performance-*, readability-*, -readability-uppercase-literal-suffix"
 	)
 endfunction()
 
